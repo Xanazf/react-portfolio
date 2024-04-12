8df3837acec06b8408ee26dd8685df21f22e9879
Xanazf <hotdamnsucka@gmail.com>
2024-04-12 03:42:39 +0300

    base tech added

--- src/main-body/backend-section/Backend.tsx
+++ src/main-body/backend-section/Backend.tsx
@@ -1,33 +1,96 @@
+import { useRef, useCallback, useEffect } from "react";
 import styles from "./Backend.module.css";
 
 const Backend = () => {
+  const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
+
+  const handleIntersection = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(styles.visible);
+        } else {
+          entry.target.classList.remove(styles.visible);
+        }
+      });
+    },
+    []
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5,
+    });
+
+    itemRefs.current.forEach((itemRef) => {
+      if (itemRef) {
+        observer.observe(itemRef);
+      }
+    });
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      itemRefs.current.forEach((itemRef) => {
+        if (itemRef) {
+          observer.unobserve(itemRef);
+        }
+      });
+    };
+  }, [handleIntersection]);
+
+  const addItemRef = useCallback((ref: HTMLLIElement | null) => {
+    if (ref) {
+      itemRefs.current.push(ref);
+    }
+  }, []);
+
   return (
     <div className={styles.backendwrap}>
       <h4>Backend</h4>
       <ul className={styles.backendlist}>
-        <li className={styles.techitem}>
-          <span>tech</span>
-          <b>0</b>
-          <div></div>
-          <b>10</b>
+        <li className={styles.techitem} ref={addItemRef}>
+          <span>
+            <img rel="icon" width={100} src="/SVG/nodejs.svg" />
+            <em>Node</em>
+          </span>
+          <b className={styles.b1}>0</b>
+          <div>
+            <wbr />
+          </div>
+          <b className={styles.b2}>10</b>
         </li>
-        <li className={styles.techitem}>
-          <span>tech</span>
-          <b>0</b>
-          <div></div>
-          <b>10</b>
+        <li className={styles.techitem} ref={addItemRef}>
+          <span>
+            <img rel="icon" width={100} src="/SVG/bash.svg" />
+            <em>Bash</em>
+          </span>
+          <b className={styles.b1}>0</b>
+          <div>
+            <wbr />
+          </div>
+          <b className={styles.b2}>10</b>
         </li>
-        <li className={styles.techitem}>
-          <span>tech</span>
-          <b>0</b>
-          <div></div>
-          <b>10</b>
+        <li className={styles.techitem} ref={addItemRef}>
+          <span>
+            <img rel="icon" width={100} src="/SVG/language-python.svg" />
+            <em>Python</em>
+          </span>
+          <b className={styles.b1}>0</b>
+          <div>
+            <wbr />
+          </div>
+          <b className={styles.b2}>10</b>
         </li>
-        <li className={styles.techitem}>
-          <span>tech</span>
-          <b>0</b>
-          <div></div>
-          <b>10</b>
+        <li className={styles.techitem} ref={addItemRef}>
+          <span>
+            <img rel="icon" width={100} src="/SVG/language-lua.svg" />
+            <em>Lua</em>
+          </span>
+          <b className={styles.b1}>0</b>
+          <div>
+            <wbr />
+          </div>
+          <b className={styles.b2}>10</b>
         </li>
       </ul>
     </div>
--- src/main-body/ci-cd-section/CICD.tsx
+++ src/main-body/ci-cd-section/CICD.tsx
@@ -1,30 +1,85 @@
+import { useRef, useCallback, useEffect } from "react";
 import styles from "./CICD.module.css";
 
 const CICD = () => {
+  const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
+
+  const handleIntersection = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(styles.visible);
+        } else {
+          entry.target.classList.remove(styles.visible);
+        }
+      });
+    },
+    []
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5,
+    });
+
+    itemRefs.current.forEach((itemRef) => {
+      if (itemRef) {
+        observer.observe(itemRef);
+      }
+    });
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      itemRefs.current.forEach((itemRef) => {
+        if (itemRef) {
+          observer.unobserve(itemRef);
+        }
+      });
+    };
+  }, [handleIntersection]);
+
+  const addItemRef = useCallback((ref: HTMLLIElement | null) => {
+    if (ref) {
+      itemRefs.current.push(ref);
+    }
+  }, []);
+
   return (
     <div className={styles.cicdwrap}>
       <h4>CI/CD</h4>
       <ul className={styles.cicdlist}>
-        <li className={styles.techitem}>
-          <span>tech</span>
+        <li className={styles.techitem} ref={addItemRef}>
+          <span>
+            <img rel="icon" width={100} src="/SVG/git.svg" />
+            <em>Git</em>
+          </span>
           <b>0</b>
           <div></div>
           <b>10</b>
         </li>
-        <li className={styles.techitem}>
-          <span>tech</span>
+        <li className={styles.techitem} ref={addItemRef}>
+          <span>
+            <img rel="icon" width={100} src="/SVG/kubernetes.svg" />
+            <em>K8s</em>
+          </span>
           <b>0</b>
           <div></div>
           <b>10</b>
         </li>
-        <li className={styles.techitem}>
-          <span>tech</span>
+        <li className={styles.techitem} ref={addItemRef}>
+          <span>
+            <img rel="icon" width={100} src="/SVG/google-cloud.svg" />
+            <em>Cloud</em>
+          </span>
           <b>0</b>
           <div></div>
           <b>10</b>
         </li>
-        <li className={styles.techitem}>
-          <span>tech</span>
+        <li className={styles.techitem} ref={addItemRef}>
+          <span>
+            <img rel="icon" width={100} src="/SVG/linux.svg" />
+            <em>Linux</em>
+          </span>
           <b>0</b>
           <div></div>
           <b>10</b>
