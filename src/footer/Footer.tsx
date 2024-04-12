4ff9efa09bbcd1edd94729b68381f6c46083b378
Xanazf <hotdamnsucka@gmail.com>
2024-04-12 15:32:27 +0300

    footer basics

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -3,7 +3,15 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.footerwrap}>
-      <span>footer</span>
+      <span>
+        <img rel="icon" width={100} src="/SVG/arch.svg" />
+      </span>
+      <span>
+        <img rel="icon" width={100} src="/SVG/vite.svg" />
+      </span>
+      <span>
+        <img rel="icon" width={100} src="/SVG/codepen.svg" />
+      </span>
     </div>
   );
 };
